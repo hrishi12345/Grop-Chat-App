4057d07cb1dd8a5fee01d2457dc2cfbf1435822a
hrishi12345 <hrajgure6@gmail.com>
2023-07-04 22:34:55 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import SignUp from "./components/users/signup";
 import Login from "./components/users/login";
+import Home from "./components/Home/Home";
 function App() {
   return (
     
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/homepage" element={<Home />}/>
       </Routes>
    
   );
--- src/components/users/login.js
+++ src/components/users/login.js
@@ -1,24 +1,40 @@
-import axios from "axios"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function Login(props){
-    const loginHandler=async (event)=>{
-        event.preventDefault()
-        const email=event.target.email.value
-        const password=event.target.password.value
-        const loginDetails={
-            email,password
-        }
-        const response=await axios.post('http://localhost:3001/user/loginuser',loginDetails)
-        console.log(response.status)
+
+function Login(props) {
+  const navigate = useNavigate()
+
+  const loginHandler = async (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const loginDetails = {
+      email,
+      password
+    };
+
+    try {
+      const response = await axios.post("http://localhost:3001/user/loginuser", loginDetails);
+
+      if (response.status === 201) {
+        alert("User Login successfully");
+        navigate("/homepage"); // Programmatically navigate to "/homepage"
+      }
+    } catch (error) {
+      console.log("Error during login:", error);
     }
-    return (
-        <form onSubmit={loginHandler}>
-            <label htmlFor="email">Email:</label>
-            <input id="email"/>
-            <label htmlFor="password">Password:</label>
-            <input id="password" />
-            <button type="submit">Submit</button>
-        </form>
-    )
+  };
+
+  return (
+    <form onSubmit={loginHandler}>
+      <label htmlFor="email">Email:</label>
+      <input id="email" />
+      <label htmlFor="password">Password:</label>
+      <input id="password" />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
-export default Login
+
+export default Login;
